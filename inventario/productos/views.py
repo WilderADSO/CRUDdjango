a8af6675c12363d304e0b0b0d10f8ebf3000a754
wilderADSO <winogaos2019@gmail.com>
2024-06-03 13:38:23 -0500

    3_tercer:Configuracion Pagina Inicio

--- inventario/productos/views.py
+++ inventario/productos/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def holaMundo(request):
     return HttpResponse("Hola Mundo ADSO")
+
+def inicio(request):
+    return render(request, 'inicio.html') 
